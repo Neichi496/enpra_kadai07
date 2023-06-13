5722d0a2744eae41d40265ea4d16f86e99267bd1
Shu <neichi496@fuji.waseda.jp>
2023-06-14 01:01:33 +0900

    Add comment + last answer output

--- git_kadai.py
+++ git_kadai.py
@@ -6,9 +6,12 @@ ans_number=0
 
 for i in range(10):
     ans_input=input("Please enter a three digit number: ")
+    #入力の判定：数字かどうか
     if ans_input.isdigit():
         ans_number=int(ans_input)
+        #入力の判定：３桁かどうか
         if ans_number>=100 and ans_number<=999:
+            #答えの正誤判定
             if ans_number==ans:
                 print('Correct! Congratulation!')
                 break
@@ -16,9 +19,16 @@ for i in range(10):
                 print('Wrong answer. The answer is a larger number')
             elif ans_number > ans:
                 print('Wrong answer. The answer is a smaller number')
+
         else:
+            #３桁の数字でない場合
             print('It is not a three digit number')
     else :
+        #数字でない入力の場合
         print('It is not a number')
+
+if i==9:
+    print("Input limit reached. unfortunately you could not enter the correct answer")
+    print("Answer is ",ans)
     
 

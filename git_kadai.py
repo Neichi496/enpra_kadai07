e1f581132c279f99d17896c8fde7ef51a2da179d
Shu <neichi496@fuji.waseda.jp>
2023-06-14 00:37:30 +0900

    while -> for (answer limit:10)

--- git_kadai.py
+++ git_kadai.py
@@ -1,16 +1,17 @@
 import random as rd
 
 ans=rd.randint(100,999)
-ans_checker='flase'
+print(ans)
 ans_number=0
 
-while ans_checker=='flase':
+for i in range(10):
     ans_input=input("Please enter a three digit number: ")
     ans_number=int(ans_input)
     if ans_number==ans:
         print('Correct! Congratulation!')
-        ans_checker='ture'
+        break
     elif ans_number < ans:
         print('Wrong answer. The answer is a larger number')
     elif ans_number > ans:
         print('Wrong answer. The answer is a smaller number')
+

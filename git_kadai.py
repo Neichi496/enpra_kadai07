5060c69cc148a502b46624e382e78c3cd49c6843
Shu <neichi496@fuji.waseda.jp>
2023-06-14 00:54:14 +0900

    Input juge (Eliminate non-numeric inputs)

--- git_kadai.py
+++ git_kadai.py
@@ -6,12 +6,19 @@ ans_number=0
 
 for i in range(10):
     ans_input=input("Please enter a three digit number: ")
-    ans_number=int(ans_input)
-    if ans_number==ans:
-        print('Correct! Congratulation!')
-        break
-    elif ans_number < ans:
-        print('Wrong answer. The answer is a larger number')
-    elif ans_number > ans:
-        print('Wrong answer. The answer is a smaller number')
+    if ans_input.isdigit():
+        ans_number=int(ans_input)
+        if ans_number>=100 and ans_number<=999:
+            if ans_number==ans:
+                print('Correct! Congratulation!')
+                break
+            elif ans_number < ans:
+                print('Wrong answer. The answer is a larger number')
+            elif ans_number > ans:
+                print('Wrong answer. The answer is a smaller number')
+        else:
+            print('It is not a three digit number')
+    else :
+        print('It is not a number')
+    
 
